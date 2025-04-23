be3e41e180dbf372c25001ec9bb1c9fd97a4b0a1
Lenne Luc <113776725+LucLenne@users.noreply.github.com>
2025-04-23 23:56:33 +0200

    Start serv

---
+++ ServerWebRasberry.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+ServerWebRasberry = Flask(__name__)
+
+commande_en_cours = {"action": "rien"}
+
+@app.route("/")
+def home():
+    return "API Pico OK"
+
+@app.route("/commande", methods=["GET"])
+def get_commande():
+    return jsonify(commande_en_cours)
+
+@app.route("/commande", methods=["POST"])
+def set_commande():
+    data = request.get_json()
+    commande_en_cours["action"] = data.get("action", "rien")
+    return jsonify({"status": "ok", "nouvelle_action": commande_en_cours["action"]})
+
+@app.route("/etat", methods=["POST"])
+def recevoir_etat():
+    data = request.get_json()
+    print("État reçu du Pico :", data)
+    return jsonify({"status": "reçu"})
