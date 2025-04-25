b41d7b15a6cd26037d1cde0a1a7abddb1a22a11d
Lenne Luc <113776725+LucLenne@users.noreply.github.com>
2025-04-26 00:40:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,24 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-commande_en_cours = {"action": "rien"}
+command = ["null","null","null","null","null","null","null"]
 
 @app.route("/")
 def home():
     return "API Pico OK"
 
-@app.route("/commande", methods=["GET"])
+@app.route("/command", methods=["GET"])
 def get_commande():
-    return jsonify(commande_en_cours)
+    return jsonify(command)
 
-@app.route("/commande", methods=["POST"])
+@app.route("/command", methods=["POST"])
 def set_commande():
     data = request.get_json()
-    commande_en_cours["action"] = data.get("action", "rien")
-    return jsonify({"status": "ok", "nouvelle_action": commande_en_cours["action"]})
+    for i in range(len(data))
+    {
+        command[i] = data[i]
+    }
+    return jsonify({"status": "ok", "nouvelle_action": command})
 
 @app.route("/etat", methods=["POST"])
 def recevoir_etat():
